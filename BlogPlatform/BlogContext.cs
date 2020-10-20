df3f7a9b86901be040ec4af785559df9fe31793f
Maureen Joyce <mjoyce10@kent.edu>
2020-10-19 22:28:50 -0400

    added seed data. first migration and updated DB

--- BlogPlatform/BlogContext.cs
+++ BlogPlatform/BlogContext.cs
@@ -24,13 +24,57 @@ namespace blog_template_practice
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Add seed data for Category model:
-            //modelBuilder.Entity<Category>().HasData(
-            //    );
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Category1"
+                },
 
-            // Add seed data for Content model:
-            //modelBuilder.Entity<Content>().HasData(
-            //    );
+                new Category
+                {
+                    Id = 2,
+                    Name = "Category2"
+                },
+
+                new Category
+                {
+                    Id = 3,
+                    Name = "Category3"
+                }
+                ); ; ;
+
+            modelBuilder.Entity<Content>().HasData(
+                new Content
+                {
+                    Id = 1,
+                    Title = "Title1",
+                    Author = "Author1",
+                    Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et bibendum sem, eu interdum mi. Sed eget elit quis mauris commodo dapibus. Integer mi turpis, commodo quis ligula non, accumsan interdum risus. Vestibulum pharetra nec felis id sollicitudin. Phasellus ex nunc, sollicitudin in semper eget, pulvinar ac nisi. Aenean venenatis velit id neque blandit egestas. Nam id elit nec mauris lacinia venenatis eget non leo. Ut in metus et lorem imperdiet eleifend. Morbi congue odio vel sapien tempus, et elementum nisl vestibulum.",
+                    PublishDate = "01/01/01",
+                    CategoryId = 1
+                },
+
+                new Content
+                {
+                    Id = 2,
+                    Title = "Title2",
+                    Author = "Author2",
+                    Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et bibendum sem, eu interdum mi. Sed eget elit quis mauris commodo dapibus. Integer mi turpis, commodo quis ligula non, accumsan interdum risus. Vestibulum pharetra nec felis id sollicitudin. Phasellus ex nunc, sollicitudin in semper eget, pulvinar ac nisi. Aenean venenatis velit id neque blandit egestas. Nam id elit nec mauris lacinia venenatis eget non leo. Ut in metus et lorem imperdiet eleifend. Morbi congue odio vel sapien tempus, et elementum nisl vestibulum.",
+                    PublishDate = "01/01/01",
+                    CategoryId = 2
+                },
+
+                new Content
+                {
+                    Id = 3,
+                    Title = "Title3",
+                    Author = "Author3",
+                    Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et bibendum sem, eu interdum mi. Sed eget elit quis mauris commodo dapibus. Integer mi turpis, commodo quis ligula non, accumsan interdum risus. Vestibulum pharetra nec felis id sollicitudin. Phasellus ex nunc, sollicitudin in semper eget, pulvinar ac nisi. Aenean venenatis velit id neque blandit egestas. Nam id elit nec mauris lacinia venenatis eget non leo. Ut in metus et lorem imperdiet eleifend. Morbi congue odio vel sapien tempus, et elementum nisl vestibulum.",
+                    PublishDate = "01/01/01",
+                    CategoryId = 2
+                }
+               );
 
             base.OnModelCreating(modelBuilder);
         }
---
+++ BlogPlatform/Migrations/20201020021948_SeedData.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlogPlatform.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Category1" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Category2" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Category3" });
+
+            migrationBuilder.InsertData(
+                table: "Contents",
+                columns: new[] { "Id", "Author", "Body", "CategoryId", "PublishDate", "Title" },
+                values: new object[] { 1, "Author1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et bibendum sem, eu interdum mi. Sed eget elit quis mauris commodo dapibus. Integer mi turpis, commodo quis ligula non, accumsan interdum risus. Vestibulum pharetra nec felis id sollicitudin. Phasellus ex nunc, sollicitudin in semper eget, pulvinar ac nisi. Aenean venenatis velit id neque blandit egestas. Nam id elit nec mauris lacinia venenatis eget non leo. Ut in metus et lorem imperdiet eleifend. Morbi congue odio vel sapien tempus, et elementum nisl vestibulum.", 1, "01/01/01", "Title1" });
+
+            migrationBuilder.InsertData(
+                table: "Contents",
+                columns: new[] { "Id", "Author", "Body", "CategoryId", "PublishDate", "Title" },
+                values: new object[] { 2, "Author2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et bibendum sem, eu interdum mi. Sed eget elit quis mauris commodo dapibus. Integer mi turpis, commodo quis ligula non, accumsan interdum risus. Vestibulum pharetra nec felis id sollicitudin. Phasellus ex nunc, sollicitudin in semper eget, pulvinar ac nisi. Aenean venenatis velit id neque blandit egestas. Nam id elit nec mauris lacinia venenatis eget non leo. Ut in metus et lorem imperdiet eleifend. Morbi congue odio vel sapien tempus, et elementum nisl vestibulum.", 2, "01/01/01", "Title2" });
+
+            migrationBuilder.InsertData(
+                table: "Contents",
+                columns: new[] { "Id", "Author", "Body", "CategoryId", "PublishDate", "Title" },
+                values: new object[] { 3, "Author3", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et bibendum sem, eu interdum mi. Sed eget elit quis mauris commodo dapibus. Integer mi turpis, commodo quis ligula non, accumsan interdum risus. Vestibulum pharetra nec felis id sollicitudin. Phasellus ex nunc, sollicitudin in semper eget, pulvinar ac nisi. Aenean venenatis velit id neque blandit egestas. Nam id elit nec mauris lacinia venenatis eget non leo. Ut in metus et lorem imperdiet eleifend. Morbi congue odio vel sapien tempus, et elementum nisl vestibulum.", 2, "01/01/01", "Title3" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Contents",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Contents",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Contents",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
