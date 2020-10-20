79225423eb4506d459ff7aa35dc92124d8efaa95
Maureen Joyce <mjoyce10@kent.edu>
2020-10-19 21:33:17 -0400

    added properties, passed tests for both models

--- BlogPlatform/Models/Category.cs
+++ BlogPlatform/Models/Category.cs
@@ -7,6 +7,14 @@ namespace blog_template_practice.Models
 {
     public class Category
     {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public virtual ICollection<Content> Contents { get; set; }
 
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 }
