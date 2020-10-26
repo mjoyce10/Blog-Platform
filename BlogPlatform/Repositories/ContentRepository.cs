5124267db27227393d80103f1b789978c8dcbb1f
Maureen Joyce <mjoyce10@kent.edu>
2020-10-26 00:30:34 -0400

    added category to content index (added to content repo to allow eager loading for 'category'). finishing touches on styling.

--- BlogPlatform/Repositories/ContentRepository.cs
+++ BlogPlatform/Repositories/ContentRepository.cs
@@ -1,4 +1,6 @@
 ﻿using blog_template_practice.Models;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,17 @@ namespace blog_template_practice.Repositories
 {
     public class ContentRepository : Repository<Content>, IRepository<Content>
     {
+        private DbContext db;
+
         public ContentRepository(BlogContext db) : base(db)
         {
+            this.db = db;
+        }
 
+
+        public override IEnumerable<Content> GetAll()
+        {
+            return db.Set<Content>().Include("Category");
         }
 
     }
